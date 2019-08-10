b95f10cf7657211797e3b73aad633072c6d028fe
WooJeongMin <minhan_a@naver.com>
2019-08-11 02:13:47 +0900

    interfaces on typescript

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-const name = "Minhan",
-      age = 27,
-      gender = "male";
+interface Human {
+  name: string;
+  age: number;
+  gender :string;
+}
 
-const sayHi = (name:string, age:number, gender?:string):string => {
- return `hi! ${name}, you are ${age}, you are a ${gender}`
+const person = {
+  name : "Minhan",
+  age : 27,
+  gender : "male"
+}
+
+
+const sayHi = (person: Human):string => {
+ return `hi! ${person.name}, you are ${person.age}, you are a ${person.gender}`
 }
 
 console.log(sayHi("Minhan", 27, "male"));
