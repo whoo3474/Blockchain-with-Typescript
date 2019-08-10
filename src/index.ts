36121b5b523fdf2ef45209fc02c5706c9b3d1656
WooJeongMin <minhan_a@naver.com>
2019-08-11 02:20:26 +0900

    classes on typescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-interface Human {
-  name: string;
-  age: number;
-  gender :string;
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender :string;
+// }
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age:number, gender:string){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
+
+const minhan = new Human("minhan", 27, "male")
 
 const person = {
   name : "Minhan",
@@ -15,7 +28,7 @@ const sayHi = (person: Human):string => {
  return `hi! ${person.name}, you are ${person.age}, you are a ${person.gender}`
 }
 
-console.log(sayHi("Minhan", 27, "male"));
+console.log(sayHi(minhan));
 
 
 export {};
