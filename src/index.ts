6039217f9b47393e3fb5e64e9416dfa312614b6f
WooJeongMin <minhan_a@naver.com>
2019-08-11 03:22:18 +0900

    blockchain creating a block

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,28 @@
-// interface Human {
-//   name: string;
-//   age: number;
-//   gender :string;
-// }
-
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age:number, gender:string){
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index:number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index:number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+    ){
+      this.index = index,
+      this.hash = hash,
+      this.previousHash = previousHash,
+      this.data = data,
+      this.timestamp = timestamp
   }
 }
 
-const minhan = new Human("minhan", 27, "male")
+const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456)
 
-const person = {
-  name : "Minhan",
-  age : 27,
-  gender : "male"
-}
+let blockchain: [Block] = [genesisBlock];
 
-
-const sayHi = (person: Human):string => {
- return `hi! ${person.name}, you are ${person.age}, you are a ${person.gender}`
-}
-
-console.log(sayHi(minhan));
-
+console.log('blockchain',blockchain)
 
 export {};
